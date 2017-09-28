a74333270a09f8fb1d863c951c0596b80687b33e
Jamas Alberta <jamesadams@outlook.com>
2017-09-28 21:11:11 +1000

    Added HelloWorld to App.js and ButtonBarney.css

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import ButtonBarney from "./ButtonBarney";
+import HelloWorld from "./HelloWorld";
 
 class App extends Component {
   render() {
@@ -16,6 +17,7 @@ class App extends Component {
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
         <ButtonBarney />
+        <HelloWorld />
       </div>
     );
   }
