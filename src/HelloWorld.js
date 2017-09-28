91289ba8c7cb345ddb67df5097e693b127ad4c61
Jamas Alberta <jamesadams@outlook.com>
2017-09-28 21:27:10 +1000

    Updated HelloWorld.js

--- src/HelloWorld.js
+++ src/HelloWorld.js
@@ -1,17 +1,7 @@
 import React from "react";
 
-const HelloWorld = props => {
-  return <div className="helloClass">Hello {props.name}</div>;
-};
-
-//it's red border and grey inside, and text is red
-
-HelloWorld.defaultProps = {
-  name: "World"
-};
-
-HelloWorld.props = {
-  name: HelloWorld.name
+const HelloWorld = ({ name = "World" }) => {
+  return <div className="helloClass">Hello {name}</div>;
 };
 
 export default HelloWorld;
