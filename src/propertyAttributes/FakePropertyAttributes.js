aceba6133d9c32597407204f3be52438595e1435
Daniel Chen <chtunsw@gmail.com>
2018-01-16 15:46:02 +1100

    1

--- src/propertyAttributes/FakePropertyAttributes.js
+++ src/propertyAttributes/FakePropertyAttributes.js
@@ -1,9 +1,6 @@
-<<<<<<< HEAD
-=======
 import React from "react";
 import FolderIcon from "material-ui-icons/Folder";
 
->>>>>>> 3fdf14464db7ae6299e81df016a40818ef5c3b3f
 const fake = [
   {
     title: "Address",
